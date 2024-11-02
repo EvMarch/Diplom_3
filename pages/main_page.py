eebdc1d24e1ebe250436ef4629109d9e747e3fc0
EvMarch <evmarch92@gmail.com>
2024-11-02 07:11:32 +0300

    4

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,5 +2,5 @@ from pages.base_page import BasePage
 from locators.main_page_locators import MainPageLocators
 import allure
 
-class MainPage(BasePage):
+#class MainPage(BasePage):
 
---
+++ pages/recovery_password_page.py
@@ -0,0 +1,3 @@
+from pages.base_page import BasePage
+from locators.main_page_locators import MainPageLocators
+import allure
